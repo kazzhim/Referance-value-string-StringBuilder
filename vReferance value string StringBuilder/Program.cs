c3c937788ff4c04eade8364a417668a0e0ddaf46
kazzhim <118839750+kazzhim@users.noreply.github.com>
2022-12-14 17:10:02 +0400

    Add files via upload

---
+++ vReferance value string StringBuilder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+string sentence = Console.ReadLine();
+
+
+StringBuilder sentence2 = new StringBuilder();
+
+string[] array = sentence.Split(' ');
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = array[i].Length - 1; j >= 0; j--)
+    {
+        sentence2.Append(array[i][j]);
+    }
+    sentence2.Append(' ');
+}
+
+
+
+//for (int i = sentence.Length - 1; i >= 0; i--)
+//{   
+
+
+//    sentence2.Append(sentence[i]);
+//}
+Console.WriteLine(sentence2);
